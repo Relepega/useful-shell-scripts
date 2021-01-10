01fce71a288eaf8a44dba71f1fef6208eb52da50
Relepega <33182302+Relepega@users.noreply.github.com>
2021-01-10 19:27:19 +0100

    Create vmware-tools-installer.sh

---
+++ vmware-tools-installer.sh
@@ -0,0 +1,22 @@
+mkdir vmwti && cd vmwti
+
+cd vmware-tools-patches
+sudo ./patched-open-vm-tools.sh
+
+cd ..
+
+sudo pacman -S asp --noconfirm
+asp checkout open-vm-tools
+cd open-vm-tools/repos/community-x86_64/
+makepkg -s --asdeps
+
+sudo cp vm* /usr/lib/systemd/system
+
+sudo systemctl enable vmware-vmblock-fuse
+sudo systemctl enable vmtoolsd
+
+cd ../../..
+
+sudo rm -rf *
+
+systemctl reboot
