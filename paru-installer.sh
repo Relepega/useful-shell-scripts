746616cfc9c376aabb4dd84a39b1c079655e03b3
Relepega <33182302+Relepega@users.noreply.github.com>
2021-01-28 14:49:09 +0100

    added paru installer

---
+++ paru-installer.sh
@@ -0,0 +1,8 @@
+echo "Paru (AUR helper) unofficial installation script"
+
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+sudo rm -rf paru
