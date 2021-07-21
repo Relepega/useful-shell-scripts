6893936aeac26a236dd3cfe511379a978d1ea981
LorenzoRizzuta <72189405+LorenzoRizzuta@users.noreply.github.com>
2021-07-21 15:15:17 +0200

    Update paru-installer.sh

--- paru-installer.sh
+++ paru-installer.sh
@@ -2,10 +2,8 @@
 
 echo "Paru (AUR helper) unofficial installation script"
 
-sudo pacman -S --needed base-devel cargo
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
+sudo pacman -S --needed --noconfirm base-devel cargo
+git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
 y
 cd ..
 sudo rm -rf paru
